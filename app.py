b412174d669246d852144584e9de89b953600e9e
666Alice666 <margosatanovskaya@gmail.com>
2026-05-27 01:05:15 +0300

    функция конфигурации

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, session
-from config import Config
+from config import CONFIG
 from modules.routes import register_routes
-import os
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['SECRET_KEY'] = CONFIG['SECRET_KEY']
 
 
 @app.before_request
@@ -16,5 +15,5 @@ def init_session():
 register_routes(app)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = CONFIG['PORT']
+    app.run(host='0.0.0.0', port=port, debug=CONFIG['DEBUG'])
--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 import os
+from decouple import config
 
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-very-secret-key'
+def get_config():
+    return {
+        'SECRET_KEY': config('SECRET_KEY', default='a-very-secret-key'),
+        'PORT': config('PORT', default=5000, cast=int),
+        'DEBUG': config('DEBUG', default=False, cast=bool),
+    }
+
+
+CONFIG = get_config()
