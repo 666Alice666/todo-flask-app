26cc4b4f2685619cc9d10d51831237c4af1f5136
666Alice666 <margosatanovskaya@gmail.com>
2026-05-27 00:19:20 +0300

    Docker

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, session
 from config import Config
 from modules.routes import register_routes
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,4 +16,5 @@ def init_session():
 register_routes(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
